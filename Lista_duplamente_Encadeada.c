8092367b662d737f0a7d41cfba80ae6621182f7d
Thomas <thomaspirescorreia@gmail.com>
2024-05-18 23:30:29 -0300

    quarto.commit

--- Lista_duplamente_Encadeada.c
+++ Lista_duplamente_Encadeada.c
@@ -1,7 +1,12 @@
+/*A lista duplamente encadeada é um tipo abstrato de dados, onde a inserção e a remoção podem ser realizadas em qualquer posição 
+da minha sequência de nós. Nesse momento teremos dois ponteiros na nossa estrutura, sendo um ponteiro que a aponta para o próximo
+nó, e um segundo ponteiro apontando para o nó anterior. Facilitando as operações de remoção e inserção.*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
+// Definição da estrutura típica dos nós que pertencem a uma lista duplamente encadeada(com dois ponteiros):
 typedef struct no{
     int info;
     struct no *prox;
@@ -9,11 +14,44 @@ typedef struct no{
 }No;
 
 
+/*(1)*/No* inserir_inicio(No *l,int valor);
+// Função que inseri os elementos no inicio da minha sequência de nós.
+
+/*(2)*/No* inserir_final(No *l,int valor);
+// Função que inseri os elementos no final da minha sequência de nós.
+
+/*(3)*/No* inserir_ordenado(No *l,int valor);
+// Função que inseri os elementos de forma ordenada na minha sequência de nós.
+
+/*(4)*/No* remover_inicio(No *l,int *valor_removido);
+// Função que remove os elementos do inicio da minha sequência de nós.
+
+/*(5)*/No* remover_final(No *l,int *valor_removido);
+// Função que remove os elementos do final da minha sequência de nós.
+
+/*(6)*/No* remover_elemento(No *l,int valor);
+// Função que remove um elemento passado como parâmetro da minha função.
+
+/*(7)*/void imprimir(No *l);
+// Função que imprimi os elementos da lista duplamente encadeada.
+
+/*(8)*/No* buscar1(No *l,int valor_procurado);
+// Função que busca um nó com um determinado elemento, se encontrado o retorna, se não retorna NULL.
+
+/*(9)*/int buscar2(No *l,int valor_procurado);
+// Função que busca um nó com um determinado elemento, se encontrado retorna 1, se não retorna 0.
+
+/*(10)*/int quant_elementos(No *l);
+// Função que conta a quantidade de elementos dentro da minha lista duplamente encadeada.
+
+/*(11)*/void liberar(No *p);
+// Função que desaloca todos os nós alocados de uma lista.
+
 int main(void) {
 
 }
 
-
+/*(1)*///FUNÇÃO DE INSERÇÃO NO INÍCIO:
 No* inserir_inicio(No *l,int valor) {
     No *novo = (No *)malloc(sizeof(No));
     novo->info = valor;
@@ -24,7 +62,7 @@ No* inserir_inicio(No *l,int valor) {
     return l;
 }
 
-
+/*(2)*///FUNÇÃO DE INSERÇÃO NO FINAL:
 No* inserir_final(No *l,int valor) {
     No *aux,*novo = (No *)malloc(sizeof(No));
     novo->info = valor;
@@ -41,7 +79,7 @@ No* inserir_final(No *l,int valor) {
     return l;
 }
 
-
+/*(3)*///FUNÇÃO DE INSERÇÃO ORDENADA:
 No* inserir_ordenado(No *l,int valor) {
 
     No *aux,*novo = (No *)malloc(sizeof(No));
@@ -73,6 +111,7 @@ No* inserir_ordenado(No *l,int valor) {
     return l;
 }
 
+/*(4)*///FUNÇÃO DE REMOÇÃO NO INÍCIO:
 No* remover_inicio(No *l,int *valor_removido) {
     No* remover;
 
@@ -85,6 +124,7 @@ No* remover_inicio(No *l,int *valor_removido) {
     return l;
 }
 
+/*(5)*///FUNÇÃO DE REMOÇÃO NO FINAL:
 No* remover_final(No *l,int *valor_removido) {
     No *remover;
     
@@ -97,6 +137,7 @@ No* remover_final(No *l,int *valor_removido) {
     return l;
 }
 
+/*(6)*///FUNÇÃO DE REMOÇÃO:
 No* remover_elemento(No *l,int valor) {
     No *remover;
 
@@ -114,6 +155,7 @@ No* remover_elemento(No *l,int valor) {
     return l;
 }
 
+/*(7)*///FUNÇÃO DE IMPRESSÃO:
 void imprimir(No *l) {
   No *aux;
 
@@ -122,7 +164,7 @@ void imprimir(No *l) {
   }
 }
 
-
+/*(8)*///FUNÇÃO DE BUSCA(retorna ponteiro):
 No* buscar1(No *l,int valor_procurado) {
     No *aux;
 
@@ -132,7 +174,7 @@ No* buscar1(No *l,int valor_procurado) {
     return NULL;
 }
 
-
+/*(9)*///FUNÇÃO DE BUSCA(retorna inteiro):
 int buscar2(No *l,int valor_procurado) {
   No *aux;
     
@@ -142,11 +184,21 @@ int buscar2(No *l,int valor_procurado) {
     return 0;
 }
 
-
+/*(10)*///FUNÇÃO DE CONTAGEM DE ELEMENTOS:
 int quant_elementos(No *l) {
     int quant=0;
     No *aux;
 
     for(aux=l;aux != NULL;aux=aux->prox,quant++);
     return quant;
+}
+
+/*(11)*///FUNÇÃO DE LIBERAÇÃO:
+void liberar(No *p) {
+  No *aux = p;
+  while(aux != NULL) {
+    p = aux->prox;
+    free(aux);
+    aux = p;
+  }
 }
