f85c4fb173190ddf71ded16f29a23a94192ff3cb
Thomas <thomaspirescorreia@gmail.com>
2024-05-20 17:26:35 -0300

    nono.commit

--- Lista_Circular_Simplismente_Encadeada.c
+++ Lista_Circular_Simplismente_Encadeada.c
@@ -13,6 +13,7 @@ int main(void) {
 
 }
 
+/*(1)*///FUNÇÃO DE INSERÇÃO NO INÍCIO:
 No* inserir_inicio(No *l,int valor) {
     No *novo = (No *)malloc(sizeof(No));
     novo->info = valor;
@@ -28,6 +29,7 @@ No* inserir_inicio(No *l,int valor) {
     return l;
 }
 
+/*(2)*///FUNÇÃO DE INSERÇÃO NO FINAL:
 No* inserir_final(No *l,int valor) {
     No *aux,*novo = (No *)malloc(sizeof(No));
     novo->info = valor;
@@ -44,6 +46,94 @@ No* inserir_final(No *l,int valor) {
     return l;
 }
 
+/*(3)*///FUNÇÃO DE INSERÇÃO ORDENADA:
 No* inserir_ordenado(No *l,int valor) {
     
+}
+
+/*(4)*///FUNÇÃO DE REMOÇÃO NO INÍCIO:
+No* remover_inicio(No *l,int *valor_removido) {
+    No *remover;
+    
+    if(l==NULL) {
+        return NULL;
+    }else{
+        remover = l->prox;
+        l->prox = remover->prox;
+        *valor_removido = remover->info;
+        free(remover);
+    }
+    return l;
+}
+
+/*(5)*///FUNÇÃO DE REMOÇÃO NO FINAL:
+No* remover_final(No *l,int *valor_removido) {
+    No *remover,*ant;
+
+    if(l==NULL) {
+        return NULL;
+    }else{
+        for(ant=l->prox;ant->prox != l;ant=ant->prox);
+        ant->prox = l->prox;
+        remover = l;
+        *valor_removido = remover->info;
+        free(remover);
+    }
+    return l;
+}
+
+
+/*(6)*///FUNÇÃO DE REMOÇÃO:
+No* remover_elemento(No *l,int valor) {
+
+}
+
+
+
+/*(7)*///FUNÇÃO DE IMPRESSÃO:
+void imprimir(No *l) {
+  No *aux;
+
+  for(aux=l;aux != NULL;aux=aux->prox) {
+    printf("%d",aux->info);
+  }
+}
+
+/*(8)*///FUNÇÃO DE BUSCA(retorna ponteiro):
+No* buscar1(No *l,int valor_procurado) {
+    No *aux;
+
+    for(aux=l;aux != NULL && aux->info != valor_procurado;aux=aux->prox);
+
+    if(aux != NULL) return aux;
+    return NULL;
+}
+
+/*(9)*///FUNÇÃO DE BUSCA(retorna inteiro):
+int buscar2(No *l,int valor_procurado) {
+  No *aux;
+    
+    for(aux=l;aux != NULL && aux->info != valor_procurado;aux=aux->prox);
+
+    if(aux != NULL) return 1;
+    return 0;
+}
+
+/*(10)*///FUNÇÃO DE CONTAGEM DE ELEMENTOS:
+int quant_elementos(No *l) {
+    int quant=0;
+    No *aux;
+
+    for(aux=l;aux != NULL;aux=aux->prox,quant++);
+    return quant;
+}
+
+/*(11)*///FUNÇÃO DE LIBERAÇÃO:
+void liberar(No *p) {
+  No *aux = p;
+  while(aux != NULL) {
+    p = aux->prox;
+    free(aux);
+    aux = p;
+  }
 }
